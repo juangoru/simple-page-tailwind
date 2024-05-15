6ec6d1240d25f636950facd2901a2224b00a664e
juangomezru <128845389+juangomezru@users.noreply.github.com>
2024-05-15 15:18:26 -0500

    Finish page with tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,40 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      textShadow: {
+        blue: "0 0 10px #00f, 0 0 15px #00f, 0 0 20px #00f, 0 0 25px #00f, 0 0 30px #00f",
+      },
+      boxShadow: {
+        purple: "0 10px 20px 0 rgba(128, 0, 128, 1)",
+      },
+      colors: {
+        "dark-background": "#1a202c",
+        "dark-primary": "#2d3748",
+        "dark-secondary": "#4a5568",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        neon: "neon 1.5s ease-in-out infinite",
+      },
+      keyframes: {
+        neon: {
+          "0%, 22%, 100%": {
+            textShadow:
+              "0 0 10px #00f, 0 0 20px #00f, 0 0 30px #00f, 0 0 40px #00f, 0 0 50px #00f, 0 0 60px #00f",
+          },
+          "28%": {
+            textShadow: "none",
+          },
+        },
+      },
     },
   },
+  darkMode: "class", // This enables dark mode
   plugins: [],
 };
+
 export default config;
